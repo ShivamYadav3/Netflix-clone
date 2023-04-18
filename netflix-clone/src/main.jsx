c3e48ff97b6b2235f122150120a0b8ba87c4d010
Shivam Yadav <shivamyadav93000@gmail.com>
2023-04-18 19:24:54 +0530

    netflix-clone

---
+++ netflix-clone/src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App'
+import "./index.css";
+// import "./App.css";
+import Row from "./component/row";
+import requests from "./component/request";
+import "./component/row.css";
+import Banner from "./component/banner";
+import Navbar from "./component/navbar";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Navbar />
+
+    <Banner />
+
+    <Row
+      title="NETFLIX ORIGINALS"
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+    />
+    <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+    <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+    <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+  </React.StrictMode>
+);
